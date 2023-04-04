8f030e4e8f7ea50dbe2b1f4b2f5e5a9ad515812f
Alexis-Orcel <115145616+alexis-orcel@users.noreply.github.com>
2023-04-04 12:37:09 +0200

    Delete job23.sql

--- job23.sql
+++
@@ -1,9 +0,0 @@
-mysql> SELECT * FROM etudiants WHERE age = (SELECT MAX(age) FROM etudiants);
-+----+-------------+--------+-----+-----------------------------+
-| id | nom         | prenom | age | email                       |
-+----+-------------+--------+-----+-----------------------------+
-|  2 | Chuck Steak | Chuck  |  45 | chuck.steak@laplateforme.io |
-|  7 | Chuck Steak | Chuck  |  45 | chuck.steak@laplateforme.io |
-| 12 | Chuck Steak | Chuck  |  45 | chuck.steak@laplateforme.io |
-+----+-------------+--------+-----+-----------------------------+
-3 rows in set (0,01 sec)
